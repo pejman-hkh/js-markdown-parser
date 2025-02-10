417ff7f868a0a9236590a5978dde0543439404ab
pejman <pejman.hkh@gmail.com>
2025-02-10 15:57:08 +0330

    some examples

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { parseMarkdown } from "./jsmp"
 
 const parser = new parseMarkdown(`
-[clear everything as **b**](https://www.google.com) test
-  `)
+[link1] [link2]
+
+# H1
+## H2
+### H3
+#### H4
+##### H5
+###### H6
+
+1. list with number
+2. list with number
+
+That's it.  Pretty simple.  There's also a drop-down option above to switch between various views:
+
+- **bold:**  list
+- **bold:**  list [link] ***bold italic***
+
+> blockquote
+
+sample text
+[link](https://www.telegram.org)
+`)
 
 const doc = parser.parse()
 console.log(doc)
