98652352952869deb5fbbe5eab2a22af7001b0a3
pejman <pejman.hkh@gmail.com>
2025-02-10 11:43:27 +0330

    some tests

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,15 @@
 import { parseMarkdown } from "./jsmp"
 
-const parser = new parseMarkdown(`**bold1**
-
-  test
-  
-  > test blockquote
-  
-  \`\`\` go
-  package main
-  
-  func main() {
-  
-  }
-  
-  \`\`\`
-  test
-  
+const parser = new parseMarkdown(`# h1 *italic*
+  _this_ is **easy** to \`use\`.
+  ~~test~~
   `)
-  
-  const doc = parser.parse()
-  console.log(doc)
-  
-  const html = parser.getHtml(doc)
-  console.log(html)
-  
-  const test = document.getElementById('app') as HTMLDivElement
-  test.innerHTML = html
+
+const doc = parser.parse()
+console.log(doc)
+
+const html = parser.getHtml(doc)
+console.log(html)
+
+const test = document.getElementById('app') as HTMLDivElement
+test.innerHTML = html
